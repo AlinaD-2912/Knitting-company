23f8893bb305fe137e4021ebf498b238a17a1873
AlinaD-2912 <dvornyk.alina2912@gmail.com>
2025-06-03 16:26:18 +0200

    le formulaire simple ajouté

---
+++ pages/contact.php
@@ -0,0 +1,15 @@
+<form class="contact-cute p-4">
+  <div class="form-group mb-3">
+    <label for="name">Your Name</label>
+    <input type="text" class="form-control" id="name" placeholder="e.g. Alice">
+  </div>
+  <div class="form-group mb-3">
+    <label for="email">Your Email</label>
+    <input type="email" class="form-control" id="email" placeholder="e.g. alice@example.com">
+  </div>
+  <div class="form-group mb-3">
+    <label for="message">Message</label>
+    <textarea class="form-control" id="message" rows="4" placeholder="How can we help you?"></textarea>
+  </div>
+  <button type="submit" class="btn btn-cart-cute"><i class="bi bi-send-heart"></i> Send</button>
+</form>
