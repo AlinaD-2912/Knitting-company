16668bae004b221977bc580613ad0f77cc72941c
AlinaD-2912 <dvornyk.alina2912@gmail.com>
2025-06-04 12:01:43 +0200

    le footer actualiser

--- footer.php
+++ footer.php
@@ -2,11 +2,9 @@
 
  <footer class="footer-cute text-light py-4 mt-auto">
   <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-    <li class="nav-item"><a href="#" class="nav-link px-3">Home</a></li>
-    <li class="nav-item"><a href="#" class="nav-link px-3">Features</a></li>
-    <li class="nav-item"><a href="#" class="nav-link px-3">Pricing</a></li>
-    <li class="nav-item"><a href="#" class="nav-link px-3">FAQs</a></li>
-    <li class="nav-item"><a href="#" class="nav-link px-3">About</a></li>
+    <li class="nav-item"><a href="?page=home" class="nav-link px-3">Home</a></li>
+    <li class="nav-item"><a href="?page=contact" class="nav-link px-3">Contact</a></li>
+    <li class="nav-item"><a href="?page=about" class="nav-link px-3">A propos</a></li>
   </ul>
   <p class="text-center mb-0">© 2025 KnitToys, made with 💙 by Alina</p>
 </footer>
